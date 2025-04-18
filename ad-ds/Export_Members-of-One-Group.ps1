436cb5746a2cbb25ff57351849b045b8ee029ebc
Stefan Salvatore <208139748+veg-salad@users.noreply.github.com>
2025-04-18 13:27:17 -0400

    Move second script to ad-ds/ directory

---
+++ ad-ds/Export_Members-of-One-Group.ps1
@@ -0,0 +1,45 @@
+<#
+This script is meant to fetch the attributes of members of a single group only...
+If you wish to perform these steps on a list of multiple groups (in same domain), check out [v1.2] of this script.
+- - -
+
+Values within angle brackets <> are to be replaced by users with actual values...
+PLEASE REMOVE ANGLE BRACKETS (<>) & DO NOT REMOVE DOUBLE QOUTES ("") WHEN YOU SUPPLY ACTUAL VALUES
+#>
+
+# Please provide name of the Active Directory <group> & its respective <domain>...
+$Group = Get-ADGroup "<group>" -Properties Members -Server <domain>
+$Domains = (Get-ADForest).Domains
+$MemberAttribs = @()
+
+foreach ($member in $Group.Members) {
+    $UserFound = $false
+
+    foreach ($domain in $Domains) {
+        try {
+            $User = Get-ADUser -Identity $member -Properties GivenName, Surname, EmailAddress, UserPrincipalName -Server $domain
+            $UserFound = $true
+            break
+        } catch [Microsoft.ActiveDirectory.Management.ADIdentityNotFoundException] {
+            continue
+        }
+    }
+
+    if ($UserFound) {
+        $Username, $UserDomain = $User.UserPrincipalName.Split('@')
+        $UserDetail = [PSCustomObject]@{
+            "First Name" = $User.GivenName
+            "Last Name" = $User.Surname
+            "Username" = $UserID
+            "Email Address" = $User.EmailAddress
+            "Domain" = $Domain
+        }
+        $UserDetails += $UserDetail
+    }
+}
+
+# This will export the data in a CSV file as "First Name, Last Name, Username, Email Address, Domain"
+# If an attribute doesn't have any value for the user in Active Directory, the entry for that column will be blank...
+$MemberAttribs | Export-Csv -Path "C:\path\to\OutputFile.csv" -NoTypeInformation
+
+# --- END OF SCRIPT ---
