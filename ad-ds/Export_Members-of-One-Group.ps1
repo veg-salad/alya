8dfd30a467969bade2e8c94a44d713645ffa6689
Stefan Salvatore <208139748+veg-salad@users.noreply.github.com>
2025-04-18 14:00:50 -0400

    Updated script ad-ds/Export_Members-of-One-Group.ps1

--- ad-ds/Export_Members-of-One-Group.ps1
+++ ad-ds/Export_Members-of-One-Group.ps1
@@ -1,10 +1,11 @@
 <#
 This script is meant to fetch the attributes of members of a single group only...
-If you wish to perform these steps on a list of multiple groups (in same domain), check out [v1.2] of this script.
+If you wish to perform these steps on a list of multiple groups (in same domain), check out "[v1.0]-Export_Members-of-Many-Groups.ps1"...
+And, if you wish to perform these steps on a list of multiple groups (of multiple domains), check out "[v1.1]-Export_Members-of-Many-Groups.ps1"
 - - -
 
 Values within angle brackets <> are to be replaced by users with actual values...
-PLEASE REMOVE ANGLE BRACKETS (<>) & DO NOT REMOVE DOUBLE QOUTES ("") WHEN YOU SUPPLY ACTUAL VALUES
+PLEASE REMOVE ANGLE BRACKETS (<>) & DO NOT REMOVE DOUBLE QUOTES ("") WHEN YOU SUPPLY ACTUAL VALUES
 #>
 
 # Please provide name of the Active Directory <group> & its respective <domain>...
