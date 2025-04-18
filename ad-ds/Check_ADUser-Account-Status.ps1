faae4bb0a82cf4997f7cce6a404fa89ce69776b4
Stefan Salvatore <208139748+veg-salad@users.noreply.github.com>
2025-04-18 13:36:12 -0400

    Updated script ad-ds/Check_ADUser-Account-Status.ps1

--- ad-ds/Check_ADUser-Account-Status.ps1
+++ ad-ds/Check_ADUser-Account-Status.ps1
@@ -1,10 +1,16 @@
-# Please provide a list of usernames only in the Users.txt input file...
+<#
+This script is meant to check status of User Accounts - whether they're Enabled or Disabled in AD.
+- - -
+#>
 
+# Please provide a list of usernames only in the Users.txt input file...
 $UserList = Get-Content -Path C:\path\to\Users.txt | ForEach-Object {
     $user = Get-ADUser -LDAPFilter "(anr=$_)" -Properties samaccountname, enabled
     $status = If ($user.enabled -eq $true) { "Active" } else { "Not Active" }
     $user | Select-Object @{Name='SAMAccountName';Expression={$_.samaccountname}}, @{Name='Status';Expression={$status}}
 }
 
-# This will export the results in a CSV file as "SAMAccountName, Status" for each user...
+# This will export the data in a CSV file as "SAMAccountName, Status" for each user...
 $UserList | Export-Csv -Path C:\path\to\OutputFile.csv -NoTypeInformation
+
+# --- END OF SCRIPT ---
